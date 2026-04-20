ea2353113fa7243256a3c6dbc259b83e3b5627bd
Muhammad Aaqib <162022683+aqibaa@users.noreply.github.com>
2026-04-20 23:24:08 +0500

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -36,8 +36,8 @@ const AboutSection = () => {
               </a>
 
               <a
-                href="public/resume/Aqib's-Resume.pdf" // The path to your resume in the public folder
-                download="Aqib's-Resume.pdf" // The filename user will see when downloading
+                href="public/resume/Aqib's-ResumeJobs (1)" 
+                download="Aqib's-ResumeJobs (1)" 
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download Resume
